d7a930ba71139857f086f105b48f3208af048741
KIM <46748877+cozyClassic@users.noreply.github.com>
2025-01-10 21:37:13 +0900

    feat: 유저 이름 사용 가능여부 확인 API 추가

--- app/user/gql_types.py
+++ app/user/gql_types.py
@@ -2,6 +2,7 @@ import strawberry
 from sqlmodel import Session
 from strawberry.types import Info
 
+from .db_models import User
 from .resolver import UserResolver
 
 
@@ -10,6 +11,13 @@ class UserType:
     user_id: int
     user_name: str
     email: str
+
+
+@strawberry.type
+class IsUserNameAvailable:
+    user_name: str
+    is_available: bool
+    description: str = "Check if the user name is available"
 
 
 @strawberry.type
@@ -23,4 +31,27 @@ class QueryUser:
             return None
         return UserType(
             user_id=user.user_id, user_name=user.user_name, email=user.email
+        )
+
+    @strawberry.field
+    def is_user_name_available(self, info: Info, user_name: str) -> IsUserNameAvailable:
+        session: Session = info.context.session
+        resolver = UserResolver(session)
+        is_available = User.validated_user_name(user_name)
+        if not is_available:
+            return IsUserNameAvailable(
+                user_name=user_name,
+                is_available=False,
+                description="User name is not Korean or not in the right length",
+            )
+
+        is_available = not (resolver.is_user_name_exist(user_name))
+        return IsUserNameAvailable(
+            user_name=user_name,
+            is_available=is_available,
+            description=(
+                "User name is available"
+                if is_available
+                else "User name is already using"
+            ),
         )
