6733c06537464dd6152a8cbf20afe9d8d0be8d8a
KIM <46748877+cozyClassic@users.noreply.github.com>
2025-01-11 02:18:36 +0900

    feat: 회원가입 API 추가

--- app/user/gql_types.py
+++ app/user/gql_types.py
@@ -1,4 +1,5 @@
 import strawberry
+from gql.gql_typs import AthleticLevel, WorkoutTime
 from sqlmodel import Session
 from strawberry.types import Info
 
@@ -54,4 +55,38 @@ class QueryUser:
                 if is_available
                 else "User name is already using"
             ),
+        )
+
+
+@strawberry.type
+class MutationUser:
+    @strawberry.mutation
+    def create_user(
+        self,
+        info: Info,
+        user_name: str,
+        email: str,
+        gender: str,
+        age: int,
+        athletic_level: AthleticLevel | None = None,
+        workout_time_prefer: WorkoutTime | None = None,
+        profile_image_url: str | None = None,
+        oauth_provider: str | None = None,
+        oauth_user_id: str | None = None,
+    ) -> UserType:
+        session: Session = info.context.session
+        resolver = UserResolver(session)
+        user = User(
+            user_name=user_name,
+            email=email,
+            gender=gender,
+            age=age,
+            athletic_level=athletic_level,
+            workout_time_prefer=workout_time_prefer,
+            profile_image_url=profile_image_url,
+        )
+        resolver.create_user_and_oauth(user, oauth_provider, oauth_user_id)
+
+        return UserType(
+            user_id=user.user_id, user_name=user.user_name, email=user.email
         )
