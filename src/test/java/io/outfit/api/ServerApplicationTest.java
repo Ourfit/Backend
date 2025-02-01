33076a18729439774da814548120adb43447bad8
SeongHo5 <newfuring1123@gmail.com>
2025-02-01 22:19:32 +0900

    Initial commit
    - Setup gradle & default packages
    - Add 1 git-crypt collaborator
    	3C200CF5 SeongHo5 <newfuring1123@gmail.com>

---
+++ src/test/java/io/outfit/api/ServerApplicationTest.java
@@ -0,0 +1,15 @@
+package io.outfit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ServerApplicationTest {
+
+  @Test
+  void contextLoads() {
+    // Test the context loading
+  }
+}
