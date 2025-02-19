73470978b5d06277b5db27ef80a23d275bb25fa5
SeongHo5 <newfuring1123@gmail.com>
2025-02-19 21:56:25 +0900

    Feat(SCRUM-14): 접근 토큰 만료시간 조정
    - 개발용

--- src/main/java/io/ourfit/api/global/jwt/impl/JwtProperties.java
+++ src/main/java/io/ourfit/api/global/jwt/impl/JwtProperties.java
@@ -1,8 +1,6 @@
 package io.ourfit.api.global.jwt.impl;
 
-import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.MacAlgorithm;
 import java.security.Key;
 import java.time.Duration;
 import java.util.Base64;
@@ -28,16 +26,13 @@ public record JwtProperties(String issuer, String secret, SecretKey key) {
   public static final String AUTHENTICATION_KEY = "auth";
 
   /** 접근 토큰의 기본 만료 시간 */
-  public static final Duration ACCESS_TOKEN_EXPIRATION = Duration.ofHours(2);
+  public static final Duration ACCESS_TOKEN_EXPIRATION = Duration.ofDays(9999);
 
   /** 갱신 토큰의 기본 만료 시간 */
   public static final Duration REFRESH_TOKEN_EXPIRATION = Duration.ofDays(14);
 
   /** 접근 토큰 갱신 시, 갱신 토큰도 자동으로 갱신하는 기준 시간 */
   public static final Duration REFRESH_TOKEN_RENEWAL_THRESHOLD = Duration.ofDays(1);
-
-  /** JWT 서명에 사용할 알고리즘 */
-  static final MacAlgorithm SIGNATURE_ALGORITHM = Jwts.SIG.HS512;
 
   @ConstructorBinding
   public JwtProperties(String issuer, String secret) {
