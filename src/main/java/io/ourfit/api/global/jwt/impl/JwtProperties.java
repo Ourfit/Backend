c7ef26f796fd3c291763de6a17a6fea2916b0ca3
SeongHo5 <newfuring1123@gmail.com>
2025-02-22 19:23:57 +0900

    Feat(SCRUM-14): 접근 토큰 만료 시간 원복

--- src/main/java/io/ourfit/api/global/jwt/impl/JwtProperties.java
+++ src/main/java/io/ourfit/api/global/jwt/impl/JwtProperties.java
@@ -26,7 +26,7 @@ public record JwtProperties(String issuer, String secret, SecretKey key) {
   public static final String AUTHENTICATION_KEY = "auth";
 
   /** 접근 토큰의 기본 만료 시간 */
-  public static final Duration ACCESS_TOKEN_EXPIRATION = Duration.ofDays(9999);
+  public static final Duration ACCESS_TOKEN_EXPIRATION = Duration.ofHours(2);
 
   /** 갱신 토큰의 기본 만료 시간 */
   public static final Duration REFRESH_TOKEN_EXPIRATION = Duration.ofDays(14);
