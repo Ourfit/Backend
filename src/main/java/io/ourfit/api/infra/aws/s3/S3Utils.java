b755f59fe40edc274d865b789f9b74afb065cc69
SeongHo5 <newfuring1123@gmail.com>
2025-02-25 11:12:25 +0900

    Fix(SCRUM-14): 파일 삭제 시 경로 체크 오류 수정

--- src/main/java/io/ourfit/api/infra/aws/s3/S3Utils.java
+++ src/main/java/io/ourfit/api/infra/aws/s3/S3Utils.java
@@ -13,9 +13,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Optional;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 public final class S3Utils {
 
   private S3Utils() {}
@@ -95,6 +98,7 @@ public final class S3Utils {
       throw new FileOperationException(ApiExceptionType.UNSUPPORTED_FILE_EXTENSION);
     }
     final var mimeType = getMimeTypeFromStream(file);
+    log.info("=======[S3Utils] {} MimeType: {}", file.getOriginalFilename(), mimeType);
     if (!AwsProperties.S3.SUPPORTED_IMAGE_MEDIA_TYPE.contains(mimeType)) {
       throw new FileOperationException(ApiExceptionType.UNSUPPORTED_MEDIA_TYPE);
     }
