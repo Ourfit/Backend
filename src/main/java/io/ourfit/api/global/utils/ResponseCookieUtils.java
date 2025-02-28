5aa65d25a933df148016f6c0c8a2c418faa388ac
SeongHo5 <newfuring1123@gmail.com>
2025-02-28 21:08:09 +0900

    Fix(SCRUM-14): 쿠키 저장 안되는 현상

--- src/main/java/io/ourfit/api/global/utils/ResponseCookieUtils.java
+++ src/main/java/io/ourfit/api/global/utils/ResponseCookieUtils.java
@@ -3,6 +3,7 @@ package io.ourfit.api.global.utils;
 import static io.ourfit.api.global.jwt.impl.JwtProperties.REFRESH_TOKEN_EXPIRATION;
 import static io.ourfit.api.global.security.SecurityConfig.REFRESH_TOKEN_COOKIE_KEY;
 
+import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.http.ResponseCookie;
 
 public final class ResponseCookieUtils {
@@ -15,9 +16,9 @@ public final class ResponseCookieUtils {
         .httpOnly(true)
         .secure(true)
         .domain(".ourfit.life")
-        //        .sameSite(SameSiteCookies.NONE.getValue())
+        .sameSite(SameSiteCookies.NONE.getValue())
         .maxAge(REFRESH_TOKEN_EXPIRATION)
-        .path("/")
+        .path("/v1/auth")
         .build();
   }
 }
